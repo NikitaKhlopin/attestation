0d650c9692b57f032981f895e9fba6b18bc631f7
NikitaKhlopin <74229659+NikitaKhlopin@users.noreply.github.com>
2020-11-13 15:46:43 +0400

    123

---
+++ 2_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+int n;
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i + j <= n)
+            {
+                cout << ' ';
+            }
+            else
+            {
+                cout << "*";
+            }
+
+        }
+        cout << "\n";
+    }
+    return 0;
+}
